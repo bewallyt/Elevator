3eae4e660de78dfa6a7f3ce7a0358f1c8b54270a
devdraweat <vmpritam@gmail.com>
2014-10-30 03:07:23 -0400

    test class

--- src/Elevator/TestElevator.java
+++ src/Elevator/TestElevator.java
@@ -1,14 +1,20 @@
 package Elevator;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class TestElevator {
 
-	public static void test(String filename) throws FileNotFoundException {
+    public static PrintStream out;
+
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		/* Run elevator */
+        out = new PrintStream(new FileOutputStream("Elevator.log"));
+        System.setOut(out);
 		Parser parser = new Parser();
-		parser.parse(filename);
+		parser.parse("ElevatorInputFile.txt");
 		
 	}
 
